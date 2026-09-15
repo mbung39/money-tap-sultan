e09d63b1f8bf76ec4abb65dfbfc18a96b9b47ff6
mbung39 <didin39scakep39@gmail.com>
2026-09-16 02:48:27 +0700

    Add POST handler for sending messages via Fonnte API

---
+++ api/kirim.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { target, message } = req.body;
+  const FONNTE_TOKEN = "ISI_TOKEN_FONNTE_LU_DISINI"; // TARUH TOKEN DI SINI BIAR AMAN
+
+  try {
+    const response = await fetch("https://api.fonnte.com/send", {
+      method: "POST",
+      headers: { 
+        "Authorization": FONNTE_TOKEN,
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: `target=${target}&message=${encodeURIComponent(message)}`
+    });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
